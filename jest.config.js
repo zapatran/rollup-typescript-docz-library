c1d6edde03705066666c8f010607db91df97d53b
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2021-02-16 12:53:54 +0100

    update

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  // transform: {
-  //   '.(ts|tsx)': 'ts-jest',
-  // },
-  // testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  // moduleFileExtensions: ['ts', 'tsx', 'js'],
   setupFilesAfterEnv: ['<rootDir>/src/setuptests.ts'],
   collectCoverage: true,
   testEnvironment: 'jsdom',
