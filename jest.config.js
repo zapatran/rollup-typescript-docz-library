8464af86095a6f0c20f1a95cba92994845d7b301
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2021-02-16 12:25:47 +0100

    update jest config and ts

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  transform: {
-    ".(ts|tsx)": "ts-jest",
+  // transform: {
+  //   '.(ts|tsx)': 'ts-jest',
+  // },
+  // testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  // moduleFileExtensions: ['ts', 'tsx', 'js'],
+  setupFilesAfterEnv: ['<rootDir>/src/setuptests.ts'],
+  collectCoverage: true,
+  testEnvironment: 'node',
+
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
   },
-  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-  moduleFileExtensions: ["ts", "tsx", "js"],
-  setupFilesAfterEnv: ["<rootDir>/src/setuptests.ts"],
 };
